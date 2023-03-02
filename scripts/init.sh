544b7025e3994d81f64dd707cc67cdfcd65b02c7
Yuri Giovani <yurigiovani.dias@gmail.com>
2023-03-01 22:04:03 -0300

    changing scripts/init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,6 +18,16 @@ output=$(echo "yes" | simd keys add "client_7" --keyring-backend $KEYRING)
 output=$(echo "yes" | simd keys add "client_8" --keyring-backend $KEYRING)
 output=$(echo "yes" | simd keys add "client_9" --keyring-backend $KEYRING)
 output=$(echo "yes" | simd keys add "client_10" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_11" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_12" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_13" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_14" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_15" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_16" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_17" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_18" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_19" --keyring-backend $KEYRING)
+output=$(echo "yes" | simd keys add "client_20" --keyring-backend $KEYRING)
 
 MY_VALIDATOR_ADDRESS=$(simd keys show $KEY -a --keyring-backend $KEYRING)
 
@@ -25,7 +35,27 @@ simd init $MONIKER --chain-id $CHAINID
 
 simd tendermint show-validator
 
-simd add-genesis-account $MY_VALIDATOR_ADDRESS 1000000000000000stake --keyring-backend $KEYRING
+simd add-genesis-account $MY_VALIDATOR_ADDRESS 500000000stake --keyring-backend $KEYRING
+
+simd add-genesis-account $(simd keys show "client_2" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_3" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_4" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_5" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_6" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_7" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_8" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_9" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_10" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_11" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_12" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_13" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_14" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_15" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_16" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_17" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_18" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_19" -a) 500000000stake  --keyring-backend $KEYRING
+simd add-genesis-account $(simd keys show "client_20" -a) 500000000stake  --keyring-backend $KEYRING
 
 # setting timeout_commit to 5 minutes
 sed -i 's/timeout_commit = "5s"/timeout_commit = "300s"/' $HOME/.simapp/config/config.toml
